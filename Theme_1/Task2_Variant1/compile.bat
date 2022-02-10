3698df198ca1ebd70db2173c746b6df33f6e5327
Marat Durdyev <marat_durdyev@epam.com>
2022-02-10 21:49:33 +0300

    Begining to work with theme_1 task_2 variant_1

---
+++ Theme_1/Task2_Variant1/compile.bat
@@ -0,0 +1,13 @@
+::::
+:: Description: .bat file to compile c++ files
+:: Autor: DM
+:: Date: 2022-02-08
+::::
+
+g++ .\main.cpp .\Person.cpp  -o program.exe
+
+@echo off
+echo --------------------------------
+echo --     Program compiled!      --
+echo --  Created program.exe file  --
+echo --------------------------------
---
+++ Theme_1/Task2_Variant1/main.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file main.cpp
+ * @author DM
+ * @brief Task_1 Variant_1
+ * @version 0.1
+ * @date 2022-02-10
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+//* Include *//
+#include <iostream>
+
+
+//* Define *//
+
+
+//* Class members *//
+
+
+//* Functions *//
+
+
+//* Main */
+int main()
+{
+	
+}
+
+//* END OF FILE *//
